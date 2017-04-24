b45cd6024ab4fff2cc00d9f4d059b84e39f1a33e
Cole Campbell <cole.campbell@twistedlogik.net>
2017-04-24 13:33:12 -0400

    Rename samples folder

---
+++ Sample14_LoadingImageDataWithSurfaces/Assets/GlobalFontID.cs
@@ -0,0 +1,9 @@
+﻿using TwistedLogik.Ultraviolet.Content;
+
+namespace UltravioletSample.Sample14_LoadingImageDataWithSurfaces.Assets
+{
+    public static class GlobalFontID
+    {
+        public static readonly AssetID SegoeUI;
+    }
+}
